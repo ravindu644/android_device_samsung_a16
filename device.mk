9bd67af394289b8705743d77fd0df831a0ea4434
adolf434 <jesusamartinezc24@gmail.com>
2025-10-14 01:05:02 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -4,25 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+LOCAL_PATH := device/samsung/a16
 
-# Boot control HAL
+# fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl \
-    android.hardware.boot@1.2-mtkimpl.recovery
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd \
+    android.hardware.health@2.0-service
 
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-    
-# MTK plpath utils
-PRODUCT_PACKAGES += \
-    mtk_plpath_utils \
-    mtk_plpath_utils.recovery
-    
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
